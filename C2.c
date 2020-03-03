3c4b011af1489476decbc47fa9658995a574a0c7
mrs1669 <mrs1669.develop@gmail.com>
2020-03-03 09:45:39 +0900

    complete C2

---
+++ C2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    int a = 10;
+    int b = 2;
+
+    printf("==========================\n");
+    printf("a + b = %d\n", a+b);
+    printf("a - b = %d\n", a-b);
+    printf("a / b = %d\n", a/b);
+    printf("a * b = %d\n", a*b);
+    printf("==========================\n");
+}
